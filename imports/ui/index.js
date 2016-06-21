e91c3461af33532d05e8cfc84e4adfda61ba50e7
tkay <aruntknambiar@gmail.com>
2016-06-21 12:12:37 +0530

    bower_components moved to public

--- imports/ui/index.js
+++ imports/ui/index.js
@@ -1,16 +1,6 @@
-//Always import these two files before any other file.
+//build file created from public/imports.html
 
-
-import "./bower_components/webcomponentsjs/webcomponents-lite.min.js";
-import "./bower_components/polymer/polymer.html";
-
-
-//Import Globally used bower Components here .
-//Also you can import all components here if you want to see them together. Another way is to import from the components which uses the element. In that way you can control the loading of dependencies by just disabling one component. check test-element.html.
-
-
-import "./bower_components/iron-flex-layout/iron-flex-layout-classes.html";
-import "./bower_components/paper-styles/paper-styles.html";
+import "./build.html";
 
 
 //Import your components
