ceb606f3657c43337c13c2444ca5b545e9a865ed
aruntk <aruntknambiar@gmail.com>
2016-12-20 17:16:40 +0530

    meteor & bower updated. #2 . moved bower components folder to imports/ui

--- imports/ui/index.js
+++ imports/ui/index.js
@@ -1,10 +1,10 @@
-//build file created from public/imports.html
+// build file created from public/imports.html
 
-import "./build.html";
-
-
-//Import your components
+import './imports.html';
 
 
-import "./layouts/test-layout.html";
-import "./components/test-element.html";
+// Import your components
+
+
+import './layouts/test-layout.html';
+import './components/test-element.html';
